d3fccdba8f0c83968ec858363f4a0d24416eb742
Kseniya-Korolchuk <kkorolchuk@gmail.com>
2018-10-25 00:49:56 +0300

    jd03_04 (project) A, B доделать

--- src/by/it/korolchuk/project/java/FrontController.java
+++ src/by/it/korolchuk/project/java/FrontController.java
@@ -9,16 +9,39 @@ import java.io.IOException;
 public class FrontController extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
         process(req, resp) ;
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
         process(req, resp);
     }
 
-    private void process (HttpServletRequest req, HttpServletResponse resp) throws IOException {
-       resp.getWriter().print("Controller is live");
+    private void process (HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+        String command = req.getParameter("command");
+        String view = "/error.jsp";
+        switch (command) {
+            case  "Index":
+                view = Actions.INDEX.JSP;
+                break;
+            case  "Login":
+                view = Actions.LOGIN.JSP;
+                break;
+            case  "Logout":
+                view = Actions.LOGOUT.JSP;
+                break;
+            case  "SignUp":
+                view = Actions.SIGNUP.JSP;
+                break;
+            case  "CreateAd":
+                view = Actions.CREATEAD.JSP;
+                break;
+            case  "ListAd":
+                view = Actions.LISTAD.JSP;
+                break;
+        }
+        getServletContext().getRequestDispatcher(view).forward(req,resp);
+
     }
 }
