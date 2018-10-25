93046665f3fe898fc39da8165180dfad0995df9b
VoiniloDmitriy <workmail1421@gmail.com>
2018-10-25 07:25:33 +0300

    project

--- src/by/it/voinilo/project/java/Actions.java
+++ src/by/it/voinilo/project/java/Actions.java
@@ -2,12 +2,18 @@ package by.it.voinilo.project.java;
 
 public enum Actions {
 
-    INDEX{{
-        this.jsp="/index.jsp";
-    }},SIGNUP
-            {{this.jsp="/index.jsp";
-            }},LOGIN{{this.jsp="/index.jsp";
-    }},LOGOUT{{this.jsp="/index.jsp";}};
+    INDEX {{
+        this.jsp = "/index.jsp";
+    }},
+    SIGNUP {{
+        this.jsp = "/signup.jsp";
+    }},
+    LOGIN {{
+        this.jsp = "/login.jsp";
+    }},
+    LOGOUT {{
+        this.jsp = "/logout.jsp";
+    }};
 
-    public String jsp= "/error.jsp";
+    public String jsp = "/error.jsp";
 }
--- src/by/it/voinilo/project/java/FrontController.java
+++ src/by/it/voinilo/project/java/FrontController.java
@@ -12,32 +12,43 @@ public class FrontController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        process(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
-        process(req,resp);
+
+        process(req, resp);
     }
 
 
-    private void process (HttpServletRequest req, HttpServletResponse response) throws IOException, ServletException {
-        process(req,response);
-        response.getWriter().print("C is alive");
+
+    private void process(HttpServletRequest req, HttpServletResponse response) throws IOException, ServletException {
+
+
         String command = req.getParameter("command");
-            String view = "/error.jsp";
-        switch (command){
+        String view = "/error.jsp";
+        switch (command) {
             case "Index":
                 view = Actions.INDEX.jsp;
+                break;
             case "Login":
                 view = Actions.LOGIN.jsp;
+                break;
             case "Logout":
                 view = Actions.LOGOUT.jsp;
+                break;
             case "SignUp":
                 view = Actions.SIGNUP.jsp;
 
-    }
-    getServletContext().getRequestDispatcher(view).forward(req,response);}
 
+        }
+
+        getServletContext().getRequestDispatcher(view).forward(req, response);
+    }
+
+    @Override
+    protected void doOptions(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
 }
