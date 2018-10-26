add52fd07af60964c61f5a8dd072d97e56bdd054
Aleksandr <nesterovich.aleksandr.a@gmail.com>
2018-10-26 19:34:50 +0300

    jd03_05 Task C (regex for sign up)

--- src/by/it/nesterovich/project/java/cmd/CmdSignUp.java
+++ src/by/it/nesterovich/project/java/cmd/CmdSignUp.java
@@ -3,6 +3,8 @@ package by.it.nesterovich.project.java.cmd;
 import by.it.nesterovich.project.java.Action;
 import by.it.nesterovich.project.java.beans.User;
 import by.it.nesterovich.project.java.dao.Dao;
+import by.it.nesterovich.project.java.ulils.Form;
+import by.it.nesterovich.project.java.ulils.Patterns;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,12 +15,12 @@ public class CmdSignUp extends Cmd {
     public Cmd execute(HttpServletRequest req, HttpServletResponse resp)
             throws Exception {
         if (req.getMethod().equalsIgnoreCase("post")) {
-            String login = req.getParameter("login");
-            String email = req.getParameter("email");
-            String password = req.getParameter("password");
-            String firstName = req.getParameter("firstName");
-            String lastName = req.getParameter("lastName");
-            int phoneNumber = Integer.parseInt(req.getParameter("phoneNumber"));
+            String login = Form.getString(req.getParameter("login"), Patterns.LOGIN);
+            String email = Form.getString(req.getParameter("email"), Patterns.EMAIL);
+            String password = Form.getString(req.getParameter("password"), Patterns.PASSWORD);
+            String firstName = Form.getString(req.getParameter("firstName"), Patterns.FIRSTNAME);
+            String lastName = Form.getString(req.getParameter("lastName"), Patterns.LASTNAME);
+            int phoneNumber = Integer.parseInt(Form.getString(req.getParameter("phoneNumber"), Patterns.PHONENUMBER));
             User user = new User(0, login, password, email, firstName, lastName, phoneNumber, 2);
             Dao dao = Dao.getDao();
             dao.user.create(user);
