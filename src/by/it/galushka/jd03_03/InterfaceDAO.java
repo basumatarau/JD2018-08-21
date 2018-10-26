2de0d50a963437d52d392d30c06eea66fad36248
Khmelov <375336849110@tut.by>
2018-10-27 00:54:10 +0300

    fix

--- src/by/it/galushka/jd03_03/InterfaceDAO.java
+++
@@ -1,9 +0,0 @@
-package by.it.galushka.jd03_03;
-
-public interface InterfaceDAO<TYPE> {
-
-    boolean create(TYPE bean);
-    TYPE read(long id);
-    boolean update(TYPE bean);
-    boolean delete(TYPE bean);
-}
