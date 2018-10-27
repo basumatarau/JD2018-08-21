b838d216cddd85402a5237917e53c76c012f2113
Галушка Игорь <galuropek@gmail.com>
2018-10-27 15:05:04 +0300

    jd03_05 fail version

--- src/by/it/galushka/project/java/Cmd.java
+++ src/by/it/galushka/project/java/Cmd.java
@@ -2,10 +2,11 @@ package by.it.galushka.project.java;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.sql.SQLException;
 
 abstract class Cmd {
 
-    abstract Cmd execute(HttpServletRequest req, HttpServletResponse resp);
+    abstract Cmd execute(HttpServletRequest req, HttpServletResponse resp) throws SQLException;
 
     @Override
     public String toString() {
--- src/by/it/galushka/project/java/CmdSignUp.java
+++ src/by/it/galushka/project/java/CmdSignUp.java
@@ -1,11 +1,25 @@
 package by.it.galushka.project.java;
 
+import by.it.galushka.project.java.beans.User;
+import by.it.galushka.project.java.dao.Dao;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.sql.SQLException;
 
 public class CmdSignUp extends Cmd {
+
     @Override
-    public Cmd execute(HttpServletRequest req, HttpServletResponse resp) {
+    public Cmd execute(HttpServletRequest req, HttpServletResponse resp) throws SQLException {
+        if (req.getMethod().equalsIgnoreCase("post")) {
+            String login = req.getParameter("login");
+            String password = req.getParameter("password");
+            String email = req.getParameter("email");
+            User user = new User(0, login, password, email, 2);
+            Dao dao = Dao.getDao();
+            dao.user.create(user);
+            return Action.SIGNUP.cmd;
+        }
         return null;
     }
 }
